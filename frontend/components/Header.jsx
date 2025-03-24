c477e27d5dc9c7fb25915eb8cd66e3f4aa629840
GautamAggarwalDev <gautam.aggarwal.tech@gmail.com>
2025-03-24 20:44:47 +0530

    create a navbar, setup auth using clerk, supabase connection

---
+++ frontend/components/Header.jsx
@@ -0,0 +1,81 @@
+import { SignedIn, SignedOut, UserButton, SignInButton } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { Button } from "./ui/button";
+import { ArrowLeft, CarFront, Heart, Layout } from "lucide-react";
+
+const Header = ({ isAdminPage = false }) => {
+  const isAdmin = false;
+  return (
+    <header className="fixed top-0 w-full bg-white/80 backdrop-blur-md z-50 border-b">
+      <nav className="mx-auto px-4 py-4 h-20 flex items-center justify-between">
+        <Link href={isAdminPage ? "/admin" : "/"} className="flex items-center">
+          <Image
+            src={"/logo.png"}
+            alt="HyperGG Rides logo"
+            width={200}
+            height={60}
+            className="h-30 w-auto object-contain rotate-6"
+          />
+          {isAdminPage && (
+            <span className="text-xs font-extralight">admin</span>
+          )}
+        </Link>
+
+        <div className="flex items-center space-x-4">
+          {isAdminPage ? (
+            <Link href="/">
+              <Button variant="outline" className="flex items-center gap-2">
+                <ArrowLeft size={18} />
+                <span className="hidden md:inline">Back to App</span>
+              </Button>
+            </Link>
+          ) : (
+            <SignedIn>
+              <Link href="/saved-cars">
+                <Button>
+                  <Heart size={18} />
+                  <span className="hidden md:inline">Saved Cars</span>
+                </Button>
+              </Link>
+              {!isAdmin ? (
+                <Link href="/reservations">
+                  <Button variant="outline">
+                    <CarFront size={18} />
+                    <span className="hidden md:inline">My Reservations</span>
+                  </Button>
+                </Link>
+              ) : (
+                <Link href="/admin">
+                  <Button variant="outline">
+                    <Layout size={18} />
+                    <span className="hidden md:inline">Admin Portal</span>
+                  </Button>
+                </Link>
+              )}
+            </SignedIn>
+          )}
+
+          <SignedOut>
+            <SignInButton>
+              <Button variant="outline">Login</Button>
+            </SignInButton>
+          </SignedOut>
+
+          <SignedIn>
+            <UserButton
+              appearance={{
+                elements: {
+                  avatarBox: "w-10 h-10",
+                },
+              }}
+            />
+          </SignedIn>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
