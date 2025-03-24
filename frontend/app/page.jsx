747ee25ba81e09a03e48fa5abfdb1f88a16b4297
GautamAggarwalDev <gautam.aggarwal.tech@gmail.com>
2025-03-24 22:48:01 +0530

    style landing page

---
+++ frontend/app/page.jsx
@@ -0,0 +1,19 @@
+
+export default function Home() {
+  return (
+    <div className="pt-20 flex flex-col">
+      {/* Hero-Section */}
+      <section className="relative py-16 md:py-28 dotted-background">
+        <div className="max-w-4xl mx-auto text-center">
+          <div className="mb-8">
+            <h1 className="text-5xl md:text-8xl mb-4 gradient-title">Find your Dream Car with HyperGG Rider</h1>
+            <p className="text-xl text-gray-200 mb-8 max-w-2xl mx-auto">
+              Advanced AI Car Search and test drive from thousands of vehicles.
+            </p>
+          </div>
+
+        </div>
+      </section>
+    </div>
+  );
+}
