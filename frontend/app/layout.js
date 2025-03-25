586bb6f9e37002867f0a2ff7315d3f9e09413465
GautamAggarwalDev <gautam.aggarwal.tech@gmail.com>
2025-03-25 20:04:07 +0530

    add Toaster

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,6 +18,7 @@ export default function RootLayout({ children }) {
         <body className={`${inter.className}`}>
           <Header />
           <main className="min-h-screen">{children}</main>
+          <Toaster richColors />
           <footer className="bg-blue-100 py-12">
             <div className="container mx-auto px-4 text-center text-gray-800">
               <p>Made by GG</p>
