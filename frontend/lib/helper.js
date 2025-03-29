bae05d9ad7485b5713d27e114c076dcf9281ba14
GautamAggarwalDev <gautam.aggarwal.tech@gmail.com>
2025-03-29 13:19:55 +0530

    fix errors

--- frontend/lib/helper.js
+++ frontend/lib/helper.js
@@ -2,9 +2,9 @@
 export const serializedCarData = (car, wishlisted = false) => {
     return {
         ...car,
-        price: car.price ? parseFloat(car.prie.toString()) : 0,
+        price: car.price ? parseFloat(car.price.toString()) : 0,
         createdAt: car.createdAt?.toISOString(),
         updatedAt: car.updatedAt?.toISOString(),
-        whishlisted: whishlisted,
+        wishlisted: wishlisted,
     };
 };
