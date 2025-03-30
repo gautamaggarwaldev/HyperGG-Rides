81e1aac19a23c253e2acc2b5e3ae9bece07816a6
GautamAggarwalDev <gautam.aggarwal.tech@gmail.com>
2025-03-30 19:07:27 +0530

    create a page for saved cars

---
+++ frontend/app/(main)/saved-cars/page.jsx
@@ -0,0 +1,30 @@
+import { getSavedCar } from '@/actions/carListing';
+import { auth } from '@clerk/nextjs/server';
+import { redirect } from 'next/navigation';
+import React from 'react'
+import SavedCarList from './_components/SavedCarList';
+
+export const metadata = {
+    title: "Saved Cars | HyperGG Rides",
+    description: "View your saved cars and favorites",
+  };
+
+const SavedCarsPage = async() => {
+
+    const { userId } = await auth();
+    if(!userId) {
+        redirect("/sign-in?redirect=/saved-cars");
+    }
+
+    const savedCarsResult = await getSavedCar();
+
+    
+  return (
+    <div className='container mx-auto px-4'>
+      <h1 className='text-6xl mb-6 gradient-title'>Your Saved Cars</h1>
+      <SavedCarList initialData={savedCarsResult} />
+    </div>
+  );
+}
+
+export default SavedCarsPage;
