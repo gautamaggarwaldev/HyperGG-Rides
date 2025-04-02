c7b661b735db858fc884a6f26a7183171fb54a6c
GautamAggarwalDev <gautam.aggarwal.tech@gmail.com>
2025-04-02 18:11:24 +0530

    add shield and bot protection

--- frontend/middleware.js
+++ frontend/middleware.js
@@ -1,4 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import arcjet, { createMiddleware, detectBot, shield } from "@arcjet/next";
+import { NextResponse } from "next/server";
 
 const isProtectedRoute = createRouteMatcher([
     "/admin(.*)",
@@ -6,14 +8,31 @@ const isProtectedRoute = createRouteMatcher([
     "/reservations(.*)",
 ]);
 
-export default clerkMiddleware(async (auth, req) => {
+const aj = arcjet({
+  key: process.env.ARCJET_KEY,
+  rules: [
+    shield({
+      mode: "LIVE",
+    }),
+    detectBot({
+      mode: "LIVE",
+      allow: ["CATEGORY:SEARCH_ENGINE"],
+    }),
+  ],
+});
+
+const clerk =  clerkMiddleware(async (auth, req) => {
     const { userId } = await auth();
 
     if(!userId && isProtectedRoute(req)) {
         const { redirectToSignIn } = await auth();
         return redirectToSignIn();
     }
+
+    return NextResponse.next();
 });
+
+export default createMiddleware(aj, clerk);
 
 export const config = {
   matcher: [
